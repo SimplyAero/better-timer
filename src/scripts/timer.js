8eddbb346874d5cda128284a7e51054fefc98066
SimplyAero <menta.matteo.m@gmail.com>
2025-11-14 12:04:48 +0100

    Implemented countdown

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -4,8 +4,6 @@ const newTimers = [];
 const unitializedTimers = [];
 const timers = [];
 
-let lastUpdate = Date.now();
-
 function handleTimeSelection(target, delta=0) {
     if (delta) {
         target.selectionStart = Math.min(
@@ -27,20 +25,26 @@ function handleTimeSelection(target, delta=0) {
     }
 }
 
+function insertTimeString(component, rawTimeString) {
+    component.value = rawTimeString.slice(0, 2) + ':'
+      + rawTimeString.slice(2, 4) + ':'
+      + rawTimeString.slice(4)
+}
+
 function normalizeTime(timer) {
     const stringChunks = []
     const display = timer.component.querySelector(".timer-display");
     stringChunks.push(timer.rawString.substring(0, 2));
     stringChunks.push(timer.rawString.substring(2, 4));
     stringChunks.push(timer.rawString.substring(4));
-    timer.time = parseInt(stringChunks[0] * 3600)
-        + parseInt(stringChunks[1] * 60)
+    timer.time = (parseInt(stringChunks[0]) * 3600)
+        + (parseInt(stringChunks[1]) * 60)
         + parseInt(stringChunks[2]);
     stringChunks[0] = Math.trunc(timer.time / 3600).toString().substring(-2).padStart(2, '0');
     stringChunks[1] = Math.trunc((timer.time % 3600) / 60).toString().substring(-2).padStart(2, '0');
     stringChunks[2] = (timer.time % 60).toString().substring(-2).padStart(2, '0');
-    display.value = stringChunks.join(':')
     timer.rawString = stringChunks.join('');
+    insertTimeString(display, timer.rawString)
 }
 
 function handleTimeTyping(timer, target, digit) {
@@ -93,6 +97,7 @@ async function createTimers() {
 
 function startTimer(timer) {
     timer.active = true;
+    timer.lastUpdate = Date.now();
     timer.component.querySelector('.timer-display').disabled = true;
 }
 
@@ -103,6 +108,7 @@ function stopTimer(timer) {
 
 function resetTimer(timer) {
     timer.currentTime = 0;
+    handleCountdown(timer);
 }
 
 function addListeners() {
@@ -137,21 +143,32 @@ function addTimer(containerSelector) {
         rawString: '000000',
         time: 0,
         currentTime: 0,
+        lastUpdate: 0,
         active: false
     });
     requestAnimationFrame(createTimers);
 }
 
 function handleCountdown(timer) {
-    timer.currentTime += (Date.now() - lastUpdate) / 1000;
-    timer.rawString = timer.component.querySelector('.timer-display').textContent;
+    const timeDelta = timer.time - timer.currentTime;
+    let raw = Math.trunc(timeDelta / 3600).toString().padStart(2, '0')
+      + Math.trunc((timeDelta % 3600) / 60).toString().padStart(2, '0')
+      + Math.trunc(timeDelta % 60).toString().padStart(2, '0');
+    timer.currentTime += (Date.now() - timer.lastUpdate) / 1000;
+    timer.lastUpdate = Date.now();
+    timer.rawString = raw.slice(-6);
+    insertTimeString(timer.component.querySelector('.timer-display'), timer.rawString);
+
 }
 
 function handleTimers() {
     for (const timer of timers.filter(timer => timer.active)) {
         handleCountdown(timer);
+        if ((timer.time - timer.currentTime) <= 0) {
+            timer.currentTime = timer.time;
+            stopTimer(timer);
+        }
     }
-    lastUpdate = Date.now();
     requestAnimationFrame(handleTimers)
 }
 
